3762ecb54265872875a74ce45712d904cb2b3cf9
kny813 <0813kny@gmail.com>
2023-02-04 20:41:10 +0800

    Update spring boot version and main application for packages scanning

--- cts/src/main/java/com/assessment/cts/CtsApplication.java
+++ cts/src/main/java/com/assessment/cts/CtsApplication.java
@@ -2,8 +2,16 @@ package com.assessment.cts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.assessment.cts.*")
+@ComponentScan("com.assessment.cts.*")
+@EntityScan("com.assessment.cts.*")
 @SpringBootApplication
+@Configuration
 public class CtsApplication {
 
 	public static void main(String[] args) {
